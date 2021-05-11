5b9bade8827ea0d8c33dbdaab211e56d9dcaef46
杨宇 <yy20@mails.tsinghua.edu.cn>
2021-05-11 20:31:23 +0800

    fix

---
+++ touteng-swift/View/BannerView.swift
@@ -0,0 +1,8 @@
+//
+//  Banner.swift
+//  touteng-swift
+//
+//  Created by Eden on 2021/5/11.
+//
+
+import Foundation
---
+++ touteng-swift/View/Page.swift
@@ -0,0 +1,20 @@
+//
+//  Page.swift
+//  touteng-swift
+//
+//  Created by Eden on 2021/5/11.
+//
+
+import SwiftUI
+
+struct Page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Page_Previews: PreviewProvider {
+    static var previews: some View {
+        Page()
+    }
+}
